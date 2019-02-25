875775b7b920d5188c2f4774dd3d1bb94686bece
jglee96 <jglee96@postech.ac.kr>
2019-02-26 00:32:52 +0900

    업데이트

--- DBR_ARLA_GPU/calRgpu.m
+++ DBR_ARLA_GPU/calRgpu.m
@@ -18,24 +18,26 @@ epst = gpuArray(epst);
 lambda = gpuArray(lambda);
 
 % calculate transfer matrix (B_tot)
-B_tot = gpuArray(zeros(2,2,length(lambda)));
-B_tot(1,1,:) = 1;
-B_tot(2,2,:) = 1;
+B_tot = gpuArray(eye(2,2));
 Pn = [epst, ones(Nstruct,1,'gpuArray')];
 Pn1 = [ones(Nstruct,1,'gpuArray'), epst];
-P = sqrt(Pn./Pn1);
+P = arrayfun(@(pn,pn1) sqrt(pn./pn1), Pn, Pn1);
 h = [dx*ones(Nstruct,Ngrid,'gpuArray'), zeros(Nstruct,1,'gpuArray')];
 Bf = @(P,kx,h) (1/2)*[(1+P)*exp(-1i*kx*h) (1-P)*exp(1i*kx*h); ...
     (1-P)*exp(-1i*kx*h) (1+P)*exp(1i*kx*h)];
-R = zeros(Nstruct,length(lambda));
+R = gpuArray(zeros(Nstruct,length(lambda)));
 
-for i = 1:Nstruct
-    for j = 1:Ngrid+1
-        for l=1:length(lambda)
-            kx = sqrt(Pn(i,j))*2*pi/(lambda(l));
-            B_tot(:,:,l) = squeeze(B_tot(:,:,l))*Bf(P(i,j),kx,h(j));
-        end
-    end
-    resultB = gather(B_tot);
-    R(i,:) = abs((resultB(2,1,:)./resultB(1,1,:))).^2;
+for l=1:length(lambda)
+    B(:,:,l) = arrayfun(@(p,h) (1+p)*h,P,h);
 end
+
+% for i = 1:Nstruct
+%     for l=1:length(lambda)
+%         for j = 1:Ngrid+1
+%             kx = sqrt(Pn(i,j))*2*pi/(lambda(l));
+%             B_tot = B_tot*Bf(P(i,j),kx,h(j));
+%         end
+%         R(i,l) = abs((B_tot(2,1)./B_tot(1,1))).^2;
+%     end
+% end
+% gather(R);

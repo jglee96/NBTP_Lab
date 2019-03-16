62147eac648d9991c6fe9f65ad577f5762365005
jglee96 <jglee96@postech.ac.kr>
2019-03-16 15:43:07 +0900

    update

--- DBR/DBR_Q_network/DBR.py
+++ DBR/DBR_Q_network/DBR.py
@@ -1,69 +1,77 @@
 import numpy as np
 
-class DBR:
-    def calR(s,Ngrid,wavelength,dx):
-        ei_element = epsi*tf.cast((s == 1),tf.int8)
-        e0_element = eps0*tf.cast((s == 0),tf.int8)
-        epst = ei_element + e0_element
 
-        Pn = np.hstack(epst,[1])
-        Pn1 = np.hstack([1],epst)
-        P = Pn/Pn1
-        kx = np.sqrt(Pn)*2*np.pi/np.transpose(wavelength)
-        h = np.hstack(np.array([dx for i in range(Ngrid)]),[0])
-        
-        P = np.vstack([P for x in range(len(wavelength))]) # extension for wavelength dependent calcaulation
-        h = np.vstack([h for x in range(len(wavelrngth))]) # extension for wavelength dependent calcaulation
+def calR(s,Ngrid,wavelength,dx,epsi,eps0):
+    ei_element = epsi*(s == 1).astype(int)
+    e0_element = eps0*(s == 0).astype(int)
+    epst = ei_element + e0_element
 
-        B11 = (1+P)*np.exp(-j*kx*h)
-        B12 = (1-P)*np.exp(j*kx*h)
-        B21 = (1-P)*np.exp(-j*kx*h) 
-        B22 = (1+P)*np.exp(j*kx*h)
+    Pn = np.hstack((epst,[[1]]))
+    Pn1 = np.hstack(([[1]],epst))
+    P = Pn/Pn1
+    kx = np.sqrt(Pn)*2*np.pi/np.transpose(wavelength)
+    h = np.hstack(([np.array([dx for i in range(Ngrid)])],[[0]]))
+    
+    P = np.vstack([P for x in range(len(wavelength))]) # extend for wavelength dependent calcaulation
+    h = np.vstack([h for x in range(len(wavelength))]) # extend for wavelength dependent calcaulation
 
-        return R
+    B11 = (1+P)*np.exp(-1j*kx*h)
+    B12 = (1-P)*np.exp(1j*kx*h)
+    B21 = (1-P)*np.exp(-1j*kx*h) 
+    B22 = (1+P)*np.exp(1j*kx*h)
 
-    def failreward():
-        Qfac = -1
-        MSL = 1
-        return (Qfac,MSL)
+    R = np.zeros(len(wavelength))
+    for w in range(len(wavelength)):
+        Btot = np.eye(2)
+        for i in range(Ngrid+1):
+            Bt = (0.5)*np.array([[B11[w,i],B12[w,i]],[B21[w,i],B22[w,i]]])
+            Btot = np.matmul(Btot,Bt)
+        R[w] = np.absolute(Btot[1,0]/Btot[0,0])**2
 
-    def reward(s,Ngrid,wavelength,R,tarwave):
-        taridx = wavelength[wavelength == tarwave]
-        tarint = R[taridx]
-        try:
-            tarhi = list(i for i in range(taridx,wavelength[-1]) if R[i] < 0.5*tarint)[0]
-            tarlo = list(i for i in range(taridx,0) if R[i] < 0.5*tarint)[0]
+    return R
 
-            if tarlo == tarhi:
-                Qfac,MSL = failreward()
+def failreward():
+    Qfac = -1
+    MSL = 1
+    return (Qfac,MSL)
 
-            else:
-                Qfac = tarint*(1/wavelength(taridx))/(1/wavelength(tarlo)-1/wavelength(tarhi))
-                MSL = np.mean(np.hstack((R[0:tarlo+1],R[tarlhi:])))            
+def reward(s,Ngrid,wavelength,R,tarwave):
+    taridx = np.where(wavelength == tarwave)
+    tarint = R[taridx]
+    try:
+        tarhi = list(i for i in range(taridx,wavelength[-1]) if R[i] < 0.5*tarint)[0]
+        tarlo = list(i for i in range(taridx,0) if R[i] < 0.5*tarint)[0]
 
-        except:
+        if tarlo == tarhi:
             Qfac,MSL = failreward()
-        
-        reward = Qfac/MSL
-        return reward
-
-
-    def step(s,Ngrid,a,dupcnt):
-        # action 0: stay
-        # action 1: go to opposite (0 -> 1, 1 -> 0) -> XOR
-
-        s = s.astype(bool)
-        a = a.astype(bool)
-
-        s1 = np.logical_xor(s,a)
-
-        if s1 == s:
-            dupcnt = dupcnt+1
-            if dupcnt == 10:
-                done = True
 
         else:
-            dupcnt = 0
+            Qfac = tarint*(1/wavelength(taridx))/(1/wavelength(tarlo)-1/wavelength(tarhi))
+            MSL = np.mean(np.hstack((R[0:tarlo+1],R[tarlhi:])))            
 
-        s1 = s1.astype(int)
-        return (s1,done,dupcnt)
+    except:
+        Qfac,MSL = failreward()
+    
+    reward = Qfac/MSL
+    return reward
+
+
+def step(s,Ngrid,a,dupcnt):
+    # action 0: stay
+    # action 1: go to opposite (0 -> 1, 1 -> 0) -> XOR
+
+    s = s.astype(bool)
+    a = a.astype(bool)
+
+    s1 = np.logical_xor(s,a)
+
+    if s1 == s:
+        dupcnt = dupcnt+1
+        if dupcnt == 10:
+            done = True
+
+    else:
+        dupcnt = 0
+
+    s1 = s1.astype(int)
+    return (s1,done,dupcnt)
--- DBR/DBR_Q_network/dbr_qn_20190314.py
+++ DBR/DBR_Q_network/dbr_qn_20190314.py
@@ -1,17 +1,19 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import DBN
+import DBR
 
 # Real world environnment
 Ngrid = 100
 dx = 10
 dbr = np.zeros(shape=[1,Ngrid],dtype=np.int8)
+epsi = 12.25
+eps0 = 1.
 
 minwave = 400
 maxwave = 1200
 wavestep = 10
-wavelength = np.arrange(minwave,maxwave,wavestep)
+wavelength = np.array([np.arange(minwave,maxwave,wavestep)])
 tarwave = 800
 
 # Input and output size based on the Env
@@ -21,11 +23,11 @@ learning_rate = 0.1
 
 # These lines establish the feed-forward part of the network used to
 # choose actions
-X = tf.placeholder(shape=[1,input_size],dtype=tf.int8) # state input
+X = tf.placeholder(shape=[1,input_size],dtype=tf.float32) # state input
 W = tf.Variable(tf.random_uniform([input_size,output_size],0,0.01)) # weight
 
 Qpred = tf.matmul(X,W) # Out Q prediction
-Y = tf.placeholder(shape=[1,output_size],dtype=tf.float16)
+Y = tf.placeholder(shape=[1,output_size],dtype=tf.float32)
 
 loss = tf.reduce_sum(tf.square(Y-Qpred))
 train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss) # Y label
@@ -44,18 +46,20 @@ with tf.Session() as sess:
         # Reser environment and get first new observation
         s = dbr
         dupcnt = 0
+        e = 1/(1+i)
+        done = False
 
         # The Q-Network training
         while not done:
             # Choose an action by greedily (with a chance of random action) from the Q-network
             Qs = sess.run(Qpred,feed_dict={X: s})
-            if np.random,rand(1) < e:
+            if np.random.rand(1) < e:
                 a = np.random.randint(-1,Ngrid)
             else:
                 a = np.argmax(Qs)
             
             # Get new state and reward
-            R = DBR.calR(s,Ngrid,wavelength,dx)
+            R = DBR.calR(s,Ngrid,wavelength,dx,epsi,eps0)
             reward = DBR.reward(s,Ngrid,wavelength,R,tarwave)
             s1,done,dupcnt = DBR.step(s,Ngrid,a,dupcnt)
 

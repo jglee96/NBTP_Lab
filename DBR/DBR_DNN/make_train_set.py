db870be704fbc6ee43a227081b7ccce51f5e0590
jglee96 <jglee96@postech.ac.kr>
2019-04-16 01:49:57 +0900

    DNN trainset update

--- DBR/DBR_DNN/make_train_set.py
+++ DBR/DBR_DNN/make_train_set.py
@@ -13,17 +13,18 @@ def beepsound():
 for i in range(10):
     sname = statefilename + '_'+i
     Rname = Rfilename + '_' + i
-for n in range(Nsample):
-    state = np.random.randint(2,size=Ngrid)
-    R = DBR.calR(state,Ngrid,wavelength,dx,epsi,eps0)
 
-    with open(sname, "a") as sf:
-        np.savetxt(sf,state,fmt='%d')
-    with open(Rname, "a") as Rf:
-        np.savetxt(Rf,R,fmt='%.5f')
+    for n in range(Nsample):
+        state = np.random.randint(2,size=Ngrid)
+        R = DBR.calR(state,Ngrid,wavelength,dx,epsi,eps0)
 
-    if (n+1)%100 == 0:
-        print(n+1,' saved')
+        with open(sname, "a") as sf:
+            np.savetxt(sf,state,fmt='%d')
+        with open(Rname, "a") as Rf:
+            np.savetxt(Rf,R,fmt='%.5f')
+
+        if (n+1)%100 == 0:
+            print(n+1,' saved')
 
 print('*****Train Set Prepared*****')
 print(beepsound())

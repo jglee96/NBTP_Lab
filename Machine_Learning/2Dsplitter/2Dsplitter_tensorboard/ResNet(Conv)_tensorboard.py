445ded184ca1647dd85265e10a529c62df9495ed
jglee96 <jglee96@postech.ac.kr>
2019-08-05 09:30:22 +0900

    ResNet(COnv

--- Machine_Learning/2Dsplitter/2Dsplitter_tensorboard/ResNet(Conv)_tensorboard.py
+++ Machine_Learning/2Dsplitter/2Dsplitter_tensorboard/ResNet(Conv)_tensorboard.py
@@ -12,6 +12,7 @@ OUTPUT_SIZE = 50
 PATH = 'D:/NBTP_Lab/Machine_Learning/2Dsplitter/2Dsplitter_tensorboard'
 TRAIN_PATH = PATH + '/trainset/12'
 
+
 def getData(): # 03
     # Load Training Data
     print("========      Load Data     ========")
@@ -57,16 +58,7 @@ def shuffle_data(X, P2):
     return X, P2
 
 
-def nll_gaussian(y_pred_mean, y_pred_sd, y_test):
-    square = tf.square(y_pred_mean - y_test)
-    ms = tf.add(tf.divide(square, 2*y_pred_sd), tf.log(2*np.pi*y_pred_sd)/2)
-    # reduce to sclar
-    ms = tf.reduce_mean(ms)
-
-    return ms
-
-
-def main(n_batch, lr_rate, beta1, beta2, n_hidden):
+def main(n_batch, lr_rate, beta1, beta2):
 
     # Load training data
     # sX, _, sP2, sP3, wavelength, Nsample = getData()
@@ -94,26 +86,65 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden):
 
     # build network
     X = tf.placeholder(tf.float32, [None, INPUT_SIZE])
+
+    ### Input Layer
+    # input: ? * 20 * 20 * 1
+    # output: ? * 20 * 20 * 1
+    net = tf.reshape(tensor=X, shape=[-1, N_pixel, N_pixel, 1], name='input_reshape')
+
+    ### Hidden Layer
+    # input: ? * 20 * 20 * 1
+    # output: ? * 20 * 20 * 16
+    conv = tf.layers.conv2d(
+        inputs=net, filters=16, kernel_size=[3, 3], padding="SAME", strides=1)
+    bm = tf.layers.batch_normalization(inputs=conv)
+    relu = tf.nn.relu(bm)
+
+    # input: ? * 20 * 20 * 1
+    # output: ? * 20 * 20 * 16
+    res1 = residual_block(relu, 16, False)
+    res2 = residual_block(res1, 16, False)
+    res3 = residual_block(res2, 16, False)
+    res4 = residual_block(res3, 16, False)
+    res5 = residual_block(res4, 16, False)
+
+    # input: ? * 20 * 20 * 16
+    # output: ? * 10 * 10 * 32
+    res6 = residual_block(res5, 32, True)
+    res7 = residual_block(res6, 32, False)
+    res8 = residual_block(res7, 32, False)
+    res9 = residual_block(res8, 32, False)
+    res10 = residual_block(res9, 32, False)
+
+    # input: ? * 10 * 10 * 32
+    # output: ? * 5 * 5 * 64
+    res11 = residual_block(res10, 64, True)
+    res12 = residual_block(res11, 64, False)
+    res13 = residual_block(res12, 64, False)
+    res14 = residual_block(res13, 64, False)
+    res15 = residual_block(res14, 64, False)
+
+    ### Global Average Pooling
+    # input: ? * 5 * 5 * 64
+    # output: ? * 1 * 1 * 64
+    gap = tf.reduce_mean(res15, [1, 2], keep_dims=True)
+
+    ### Output Layers
+    # input: ? * 1 * 1 * 64
+    # output: ? * 1 * 1 * 64
+    shape = gap.get_shape().as_list()
+    dimension = shape[1] * shape[2] * shape[3]
+    flat = tf.reshape(gap, [-1, dimension])
+
+    Yhat = tf.layers.dense(flat, OUTPUT_SIZE, activation=None, name="Yhat")
+
     Y = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
-
-    net = tf.layers.dense(X, n_hidden[0], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="transe")
-    for i, n in enumerate(n_hidden):
-        prenet = net
-        with tf.name_scope('dense'+str(i)+'_1'):
-            net = tf.layers.dense(net, n, activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense"+str(i)+'_1')
-        with tf.name_scope('dense'+str(i)+'_2'):
-            net = tf.layers.dense(net, n, activation=None, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="dense"+str(i)+'_2')
-        net = tf.nn.relu(tf.add(net, prenet))
-
-    Yhat = tf.layers.dense(net, OUTPUT_SIZE, activation=None, name="Yhat")
-
     # loss = tf.reduce_mean(tf.square(Y - Yhat))
     loss = tf.reduce_mean(tf.abs(Y - Yhat))
-    # loss = nll_gaussian(Y, tf.exp(Y), Yhat)
     testloss = tf.reduce_mean(tf.abs(Y - Yhat))
     loss_hist = tf.summary.scalar('loss', loss)
     train = tf.train.AdamOptimizer(learning_rate=lr_rate).minimize(loss)
-    
+
     with tf.Session() as sess:
         # tensorboard
         net.writer = tf.summary.FileWriter(PATH + '/logs/'+datetime.now().strftime("%Y%m%d%H%M"))
@@ -129,9 +160,9 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden):
             feed_train = {X: feed_trainX, Y: feed_trainY}
             sess.run(train, feed_dict=feed_train)
             # log
-            if n%10 == 0:
+            if n % 10 == 0:
                 merged_summary = tf.summary.merge([loss_hist])
-                summary =sess.run(merged_summary, feed_dict=feed_train)
+                summary = sess.run(merged_summary, feed_dict=feed_train)
                 net.writer.add_summary(summary, global_step=n)
                 print(n, 'trained')
 
@@ -156,8 +187,33 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden):
         plt.show()
 
 
-def batch_norm(x, is_training=True, scope='batch_norm'):
-    return tf.contrib.layers.batch_norm(x, decay=0.9, epsilon=1e-05, center=True, scale=True, updates_collections=None, is_training=is_training, scope=scope)
+def residual_block(X, num_filter, chg_dim):
+    stride = 1
+
+    if chg_dim:
+        stride = 2
+        pool1 = tf.layers.max_pooling2d(inputs=X, strides=2, pool_size=[1, 1])
+        pad1 = tf.pad(pool1, [[0, 0], [0, 0], [0, 0], [int(num_filter/4), int(num_filter/4)]])
+        shortcut = pad1
+    else:
+        shortcut = X
+
+    conv1 = tf.layers.conv2d(
+        inputs=X, filters=num_filter, kernel_size=[3, 3], padding='SAME',
+        strides=stride, kernel_initializer=tf.contrib.layers.xavier_initializer())
+    bm1 = tf.layers.batch_normalization(inputs=conv1)
+    relu1 = tf.nn.relu(bm1)
+
+    conv2 = tf.layers.conv2d(
+        inputs=relu1, filters=num_filter, kernel_size=[3, 3],
+        padding='SAME', strides=1, kernel_initializer=tf.contrib.layers.xavier_initializer())
+    bm2 = tf.layers.batch_normalization(inputs=conv2)
+
+    X_output = tf.nn.relu(bm2 + shortcut)
+
+    return X_output
+
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
         description="Physics Net Training")
@@ -165,7 +221,6 @@ if __name__=="__main__":
     parser.add_argument("--lr_rate", type=float, default=1E-3)
     parser.add_argument("--beta1", type=float, default=0.9)
     parser.add_argument("--beta2", type=float, default=0.7)
-    parser.add_argument("--n_hidden", default=[200, 200, 200])
     args = parser.parse_args()
     dict = vars(args)
 
@@ -187,8 +242,7 @@ if __name__=="__main__":
             'n_batch':dict['n_batch'],
             'lr_rate':dict['lr_rate'],
             'beta1':dict['beta1'],
-            'beta2':dict['beta2'],
-            'n_hidden':dict['n_hidden']
+            'beta2':dict['beta2']
             }
 
     main(**kwargs)

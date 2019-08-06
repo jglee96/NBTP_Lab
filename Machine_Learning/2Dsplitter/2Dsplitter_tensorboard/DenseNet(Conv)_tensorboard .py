92638b3775e497901dc314d09a10c01e82731c36
jglee96 <jglee96@postech.ac.kr>
2019-08-06 22:17:17 +0900

    densenet update

--- Machine_Learning/2Dsplitter/2Dsplitter_tensorboard/DenseNet(Conv)_tensorboard .py
+++ Machine_Learning/2Dsplitter/2Dsplitter_tensorboard/DenseNet(Conv)_tensorboard .py
@@ -3,17 +3,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+from tflearn.layers.conv import global_avg_pool
 from datetime import datetime
 
 N_pixel = 20
 INPUT_SIZE = N_pixel * N_pixel
 OUTPUT_SIZE = 50
 
-PATH = 'D:/LJG/2Dsplitter'
-TRAIN_PATH = PATH + '/trainset/10'
+PATH = 'D:/NBTP_Lab/Machine_Learning/2Dsplitter/2Dsplitter_tensorboard'
+TRAIN_PATH = PATH + '/trainset/11'
 
 
-def getData():
+def getData(): # 03
     # Load Training Data
     print("========      Load Data     ========")
 
@@ -51,14 +52,17 @@ def shuffle_data(X, P2):
     np.random.shuffle(x)
 
     X = X[x, :]
+    # P1 = P1[x, :]
     P2 = P2[x, :]
+    # P3 = P3[x, :]
 
     return X, P2
 
 
-def main(n_batch, lr_rate, beta1, beta2, n_hidden, n_block):
+def main(n_batch, lr_rate, beta1, beta2):
 
     # Load training data
+    # sX, _, sP2, sP3, wavelength, Nsample = getData()
     sX, sP1, sP2, sP3, Nsample = getData()
 
     Nr = 0.9
@@ -66,54 +70,33 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden, n_block):
     Ntest = int((1-Nr)*Nsample)
 
     trainX = sX[0:Nlearning, :]
+    # trainP1 = sP1[0:Nlearning, :]
     trainP2 = sP2[0:Nlearning, :]
+    # trainP3 = sP3[0:Nlearning, :]
     trainX_total = trainX
+    # trainP1_total = trainP1
     trainP2_total = trainP2
-
+    # trainP3_total = trainP3
     n_copy = 100
     for i in range(n_copy):
         trainX, trainP2 = shuffle_data(trainX, trainP2)
         trainX_total = np.concatenate((trainX_total, trainX), axis=0)
+        # trainP1_total = np.concatenate((trainP1_total, trainP1), axis=0)
         trainP2_total = np.concatenate((trainP2_total, trainP2), axis=0)
+        # trainP3_total = np.concatenate((trainP3_total, trainP3), axis=0)
 
-    with tf.device('/GPU:0'):
-        # build network
-        X = tf.placeholder(tf.float32, [None, INPUT_SIZE])
-        Y = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
+    # build network
+    X = tf.placeholder(tf.float32, [None, INPUT_SIZE])
+    net = tf.reshape(tensor=X, shape=[-1, N_pixel, N_pixel, 1], name='input_reshape')
 
-    with tf.device('/GPU:0'):
-        net = tf.layers.dense(X, n_hidden[0], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name="transe")
-    net_hist = []
-    for idx, n in enumerate(n_block):
-        dense = []
-        dense.append(net)
-        for i in range(n):
-            with tf.name_scope('dense'+str(idx)+'_'+str(i)):
-                with tf.device('/GPU:0'):
-                    net = tf.layers.dense(net, n_hidden[idx], activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer(), name='dense'+str(idx)+'_'+str(i))
-                dense.append(net)
-                net_hist.append(tf.summary.histogram("activations"+str(idx)+'_'+str(i), net))
-                dense_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "dense"+str(idx)+'_'+str(i))
-                net_hist.append(tf.summary.histogram("weights"+str(idx)+'_'+str(i), dense_vars[0]))
-                net_hist.append(tf.summary.histogram("biases"+str(idx)+'_'+str(i), dense_vars[1]))
-        with tf.device('/GPU:0'):
-            net = tf.concat(dense, axis=0)
-            net = tf.nn.relu(net)
+    Yhat = Dense_net(X=net, num_filter=10, nb_blocks=[6])
+    Y = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
 
-    with tf.name_scope('Yhat'):
-        with tf.device('/GPU:0'):
-            Yhat = tf.layers.dense(net, OUTPUT_SIZE, activation=None, name="Yhat")
-        net_hist.append(tf.summary.histogram("activations_Yhat", Yhat))
-        dense_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Yhat")
-        net_hist.append(tf.summary.histogram("weights_Yhat", dense_vars[0]))
-        net_hist.append(tf.summary.histogram("biasess_Yhat", dense_vars[1]))
-
-    with tf.device('/GPU:0'):
-        # loss = tf.reduce_mean(tf.square(Y - Yhat))
-        loss = tf.reduce_mean(tf.abs(Y - Yhat))
-        testloss = tf.reduce_mean(tf.abs(Y - Yhat))
-        train = tf.train.AdamOptimizer(learning_rate=lr_rate).minimize(loss)
+    # loss = tf.reduce_mean(tf.square(Y - Yhat))
+    loss = tf.reduce_mean(tf.abs(Y - Yhat))
+    testloss = tf.reduce_mean(tf.abs(Y - Yhat))
     loss_hist = tf.summary.scalar('loss', loss)
+    train = tf.train.AdamOptimizer(learning_rate=lr_rate).minimize(loss)
 
     with tf.Session() as sess:
         # tensorboard
@@ -130,9 +113,9 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden, n_block):
             feed_train = {X: feed_trainX, Y: feed_trainY}
             sess.run(train, feed_dict=feed_train)
             # log
-            if n%10 == 0:
-                merged_summary = tf.summary.merge([loss_hist] + net_hist)
-                summary =sess.run(merged_summary, feed_dict=feed_train)
+            if n % 10 == 0:
+                merged_summary = tf.summary.merge([loss_hist])
+                summary = sess.run(merged_summary, feed_dict=feed_train)
                 net.writer.add_summary(summary, global_step=n)
                 print(n, 'trained')
 
@@ -156,6 +139,72 @@ def main(n_batch, lr_rate, beta1, beta2, n_hidden, n_block):
         plt.plot(np.reshape(sP2[99, :], OUTPUT_SIZE))
         plt.show()
 
+
+def bottleneck_layer(X, num_filter, scope):
+    with tf.name_scope(scope):
+        net = tf.layers.batch_normalization(inputs=X)
+        net = tf.nn.relu(net)
+        net = tf.layers.conv2d(
+            inputs=net, filters=4*num_filter, kernel_size=[1, 1], padding='SAME',
+            strides=1, kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+        net = net = tf.layers.batch_normalization(inputs=net)
+        net = tf.nn.relu(net)
+        net = tf.layers.conv2d(
+            inputs=net, filters=4*num_filter, kernel_size=[3, 3], padding='SAME',
+            strides=1, kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+        return net
+
+
+def transition_layer(X, scope):
+    with tf.name_scope(scope):
+        net = tf.layers.batch_normalization(inputs=X)
+        net = tf.nn.relu(net)
+
+        in_channel = X.shape[-1].value
+        net = tf.layers.conv2d(
+            inputs=net, filters=int(in_channel*0.5), kernel_size=[1, 1], padding='SAME',
+            strides=2, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        net = tf.layers.average_pooling2d(inputs=net, pool_size=[2, 2], strides=2, padding='VALID')
+
+        return net
+
+
+def dense_block(X, num_filter, nb_layers, layer_name):
+    with tf.name_scope(layer_name):
+        layers_concat = list()
+        layers_concat.append(X)
+
+        net = bottleneck_layer(X, num_filter, scope=layer_name+'_bottleN_' + str(0))
+
+        layers_concat.append(net)
+
+        for i in range(nb_layers - 1):
+            net = tf.concat(layers_concat, axis=3)
+
+        return net
+
+
+def Dense_net(X, num_filter, nb_blocks):
+    conv = tf.layers.conv2d(
+        inputs=X, filters=2*num_filter, kernel_size=[3, 3], padding='SAME',
+        strides=2, kernel_initializer=tf.contrib.layers.xavier_initializer())
+    net = tf.layers.max_pooling2d(inputs=X, strides=2, pool_size=[3, 3], padding='VALID')
+
+    for idx, i in enumerate(nb_blocks):
+        net = dense_block(X=net, num_filter=num_filter, nb_layers=i, layer_name='dense_'+str(idx))
+        net = transition_layer(net, scope='trans_'+str(idx))
+
+    net = tf.layers.batch_normalization(inputs=net)
+    net = tf.nn.relu(net)
+    net = global_avg_pool(net, name='Global_avg_pooling')
+    net = tf.contrib.layers.flatten(net)
+    net = tf.layers.dense(inputs=net, units=OUTPUT_SIZE, name='linear')
+
+    return net
+
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
         description="Physics Net Training")
@@ -163,8 +212,6 @@ if __name__=="__main__":
     parser.add_argument("--lr_rate", type=float, default=1E-3)
     parser.add_argument("--beta1", type=float, default=0.9)
     parser.add_argument("--beta2", type=float, default=0.7)
-    parser.add_argument("--n_hidden", default=[100, 100, 100])
-    parser.add_argument("--n_block", default=[6, 12, 48])
     args = parser.parse_args()
     dict = vars(args)
 
@@ -186,9 +233,7 @@ if __name__=="__main__":
             'n_batch':dict['n_batch'],
             'lr_rate':dict['lr_rate'],
             'beta1':dict['beta1'],
-            'beta2':dict['beta2'],
-            'n_hidden':dict['n_hidden'],
-            'n_block':dict['n_block']
+            'beta2':dict['beta2']
             }
 
     main(**kwargs)
